0e9cc73e6c57de3bb082d0fbda2f37454b77a3ce
ines-mgg <i.maganga@yahoo.com>
2024-07-09 12:15:44 +0200

    fix issue with @type:offer

--- sensvinylo/pages/index.vue
+++ sensvinylo/pages/index.vue
@@ -38,12 +38,42 @@ useSchemaOrg([
           description: "Après son dernier single \"feather\", \"espresso\" est le prochain triomphe de Sabrina. Vinyle de 7 pouces en brun chocolat. Veuillez noter que la couleur du vinyle peut varier légèrement par rapport à la photo.",
           sku: 'sabrina-espresso',
           brand: 'Sabrina Carpenter',
+          aggregateRating: {
+            '@type': 'AggregateRating',
+          },
+          review: {
+            '@type': 'Review',
+          },
           offers: {
             '@type': 'Offer',
             price: '10.99',
             priceCurrency: 'EUR',
             availability: 'InStock',
+            priceValidUntil: '2024-12-31',
             url: 'https://sensvinylo-store.com/vinyle/sabrina-espresso',
+            shippingDetails: {
+              '@type': 'OfferShippingDetails',
+              deliveryTime: {
+                "@type": "ShippingDeliveryTime",
+                businessDays: "1-2",
+              },
+              cutoffTime: "12:00:15Z",
+              shippingRate: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '4.65',
+              },
+            },
+            hasMerchantReturnPolicy: {
+              '@type': 'MerchantReturnPolicy',
+              refundType: 'FullRefund',
+              returnPolicyCategory: 'InStoreReturnPolicy',
+              restockingFee: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '0.00',
+              },
+            },
           },
         },
       },
@@ -58,12 +88,42 @@ useSchemaOrg([
           description: "Did you know that there's a tunnel under Ocean Blvd est le neuvième album studio de la célèbre auteure-compositrice-interprète Lana Del Rey. Il comprend 16 titres et des interludes, avec notamment Father John Misty, Tommy Genesis, Jon Batiste, Bleachers et bien d'autres. Il fait suite aux albums Chemtrails Over The Country Club et Blue Banisters, sortis en 2021. La version festival en édition limitée comprend un artwork alternatif et un vinyle orange \"heavyweight\".",
           sku: 'lana-ocean-blvd',
           brand: 'Lana Del Rey',
+          aggregateRating: {
+            '@type': 'AggregateRating',
+          },
+          review: {
+            '@type': 'Review',
+          },
           offers: {
             '@type': 'Offer',
             price: '39.99',
             priceCurrency: 'EUR',
             availability: 'InStock',
+            priceValidUntil: '2024-12-31',
             url: 'https://sensvinylo-store.com/vinyle/lana-ocean-blvd',
+            shippingDetails: {
+              '@type': 'OfferShippingDetails',
+              deliveryTime: {
+                "@type": "ShippingDeliveryTime",
+                businessDays: "1-2",
+              },
+              cutoffTime: "12:00:15Z",
+              shippingRate: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '4.65',
+              },
+            },
+            hasMerchantReturnPolicy: {
+              '@type': 'MerchantReturnPolicy',
+              refundType: 'FullRefund',
+              returnPolicyCategory: 'InStoreReturnPolicy',
+              restockingFee: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '0.00',
+              },
+            },
           },
         },
       },
@@ -78,12 +138,42 @@ useSchemaOrg([
           description: "En avril 2022, AURORA a lu une lettre qui a changé sa vie. Elle a été co-écrite par des militants autochtones, intitulé « Nous sommes la Terre », et appelait à une révolution : une réponse collective au réchauffement climatique – pour « guérir la terre ». Ils ont décrit être connectés à la terre « à travers notre cœur » et à la terre comme « le cœur qui palpite en nous ». La lettre a amené AURORA à réfléchir à une question : What Happened To The Heart? C’est ainsi qu’elle a commencé à étudier des livres sur l’anatomie humaine, voulant comprendre quand et pourquoi la culture occidentale a perdu contact avec le but le plus profond de notre organe le plus vital. What Happened To The Heart? est un voyage de la faiblesse à la force, de l'autodestruction à l'auto-guérison. Un voyage musical poignant et introspectif qui explore la perte de lien spirituel dans la société moderne, le pouvoir de guérison de la vulnérabilité et l'appel à déclencher le changement en réunissant le cœur avec la politique et la croissance personnelle.",
           sku: 'aurora-heart',
           brand: 'AURORA',
+          aggregateRating: {
+            '@type': 'AggregateRating',
+          },
+          review: {
+            '@type': 'Review',
+          },
           offers: {
             '@type': 'Offer',
             price: '37.99',
             priceCurrency: 'EUR',
             availability: 'InStock',
+            priceValidUntil: '2024-12-31',
             url: 'https://sensvinylo-store.com/vinyle/aurora-heart',
+            shippingDetails: {
+              '@type': 'OfferShippingDetails',
+              deliveryTime: {
+                "@type": "ShippingDeliveryTime",
+                businessDays: "1-2",
+              },
+              cutoffTime: "12:00:15Z",
+              shippingRate: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '4.65',
+              },
+            },
+            hasMerchantReturnPolicy: {
+              '@type': 'MerchantReturnPolicy',
+              refundType: 'FullRefund',
+              returnPolicyCategory: 'InStoreReturnPolicy',
+              restockingFee: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '0.00',
+              },
+            },
           },
         },
       },
@@ -98,12 +188,42 @@ useSchemaOrg([
           description: "Vinyle édition limitée de l'album Æ de Booba. Disponible en rouge.",
           sku: 'booba-ae',
           brand: 'Booba',
+          aggregateRating: {
+            '@type': 'AggregateRating',
+          },
+          review: {
+            '@type': 'Review',
+          },
           offers: {
             '@type': 'Offer',
             price: '27.99',
             priceCurrency: 'EUR',
             availability: 'InStock',
+            priceValidUntil: '2024-12-31',
             url: 'https://sensvinylo-store.com/vinyle/booba-ae',
+            shippingDetails: {
+              '@type': 'OfferShippingDetails',
+              deliveryTime: {
+                "@type": "ShippingDeliveryTime",
+                businessDays: "1-2",
+              },
+              cutoffTime: "12:00:15Z",
+              shippingRate: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '4.65',
+              },
+            },
+            hasMerchantReturnPolicy: {
+              '@type': 'MerchantReturnPolicy',
+              refundType: 'FullRefund',
+              returnPolicyCategory: 'InStoreReturnPolicy',
+              restockingFee: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '0.00',
+              },
+            },
           },
         },
       },
@@ -118,12 +238,42 @@ useSchemaOrg([
           description: "Au cours des six dernières années, Zayn a écrit son quatrième album studio dans son studio en milieu rural en Pennsylvanie. Cet album marque sa sortie la plus personnelle à ce jour, reflétant où il en est dans sa vie tout en explorant les complexités de la guérison, du calme et de la croissance. Il voit également l'artiste qui plie les genres explorer un nouveau son, en s'appuyant sur sa voix émouvante, ses instruments live et son lyrisme poétique en tant qu'auteur-compositeur. L'album a été coproduit par Zayn avec le producteur de disques Dave Cobb, lauréat de 9 Grammy Awards. Ce sera le premier album de Zayn avec Mercury Records.",
           sku: 'zayn-room',
           brand: 'Zayn',
+          aggregateRating: {
+            '@type': 'AggregateRating',
+          },
+          review: {
+            '@type': 'Review',
+          },
           offers: {
             '@type': 'Offer',
             price: '44.99',
             priceCurrency: 'EUR',
             availability: 'InStock',
+            priceValidUntil: '2024-12-31',
             url: 'https://sensvinylo-store.com/vinyle/zayn-room',
+            shippingDetails: {
+              '@type': 'OfferShippingDetails',
+              deliveryTime: {
+                "@type": "ShippingDeliveryTime",
+                businessDays: "1-2",
+              },
+              cutoffTime: "12:00:15Z",
+              shippingRate: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '4.65',
+              },
+            },
+            hasMerchantReturnPolicy: {
+              '@type': 'MerchantReturnPolicy',
+              refundType: 'FullRefund',
+              returnPolicyCategory: 'InStoreReturnPolicy',
+              restockingFee: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '0.00',
+              },
+            },
           },
         },
       },
@@ -138,12 +288,42 @@ useSchemaOrg([
           description: "La bande originale officielle du nouveau film d'Amy Winehouse, BACK TO BLACK, qui sortira au printemps 2024. Amy Winehouse est largement considérée comme l'une des plus grandes artistes de l'histoire récente.  Avec Plus de 30 millions de disques vendus dans le monde, son album Back to Black, autoproduit et acclamé, sorti en 2006, l'a propulsée au rang de star mondiale et lui a permis de remporter 5 Grammy Awards (un record à l'époque), dont celui du disque de l'année et celui de la chanson de l'année pour pour le single Rehab. Sorti sur un vinyle 1LP Couleur de 140g, couleur pêche, la liste des titres comprend une collection d'enregistrements originaux d'Amy et des morceaux de ses idoles, telles que The Shangri-Las, Billie Holiday et Dinah Washington, qui figurent dans le film, ainsi qu'un nouveau titre original chanté par Nick Cave. Le produit en vinyle est présenté dans une pochette simple avec des intérieurs imprimés comportant une note personnelle du réalisateur, Sam Taylor-Johnson.",
           sku: 'amy-back-black',
           brand: 'Amy Winehouse',
+          aggregateRating: {
+            '@type': 'AggregateRating',
+          },
+          review: {
+            '@type': 'Review',
+          },
           offers: {
             '@type': 'Offer',
             price: '29.99',
             priceCurrency: 'EUR',
             availability: 'InStock',
+            priceValidUntil: '2024-12-31',
             url: 'https://sensvinylo-store.com/vinyle/amy-back-black',
+            shippingDetails: {
+              '@type': 'OfferShippingDetails',
+              deliveryTime: {
+                "@type": "ShippingDeliveryTime",
+                businessDays: "1-2",
+              },
+              cutoffTime: "12:00:15Z",
+              shippingRate: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '4.65',
+              },
+            },
+            hasMerchantReturnPolicy: {
+              '@type': 'MerchantReturnPolicy',
+              refundType: 'FullRefund',
+              returnPolicyCategory: 'InStoreReturnPolicy',
+              restockingFee: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '0.00',
+              },
+            },
           },
         },
       },
@@ -158,12 +338,42 @@ useSchemaOrg([
           description: "L'artiste d'enregistrement primée à trois reprises aux GRAMMY®, Olivia Rodrigo, réédite GUTS sur un vinyle de luxe en édition extrêmement limitée. logé dans une veste gatefold, Présentant des illustrations retravaillées et 5 titres supplémentaires, ce vinyle éclaboussé en édition spéciale est le seul pressage contenant la collection complète des chansons de GUTS. Disponible en précommande maintenant. GUTS a été enregistré avec le producteur Daniel Nigro, qui a également collaboré avec Olivia sur SOUR, son premier album en tête des charts, 4x platine. \"Pour moi, cet album parle de douleurs de croissance et d'essayer de comprendre qui je suis à ce stade de ma vie et exactement ce que je veux dire dans mes chansons.\"",
           sku: 'olivia-guts',
           brand: 'Olivia Rodrigo',
+          aggregateRating: {
+            '@type': 'AggregateRating',
+          },
+          review: {
+            '@type': 'Review',
+          },
           offers: {
             '@type': 'Offer',
             price: '39.99',
             priceCurrency: 'EUR',
             availability: 'InStock',
+            priceValidUntil: '2024-12-31',
             url: 'https://sensvinylo-store.com/vinyle/olivia-guts',
+            shippingDetails: {
+              '@type': 'OfferShippingDetails',
+              deliveryTime: {
+                "@type": "ShippingDeliveryTime",
+                businessDays: "1-2",
+              },
+              cutoffTime: "12:00:15Z",
+              shippingRate: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '4.65',
+              },
+            },
+            hasMerchantReturnPolicy: {
+              '@type': 'MerchantReturnPolicy',
+              refundType: 'FullRefund',
+              returnPolicyCategory: 'InStoreReturnPolicy',
+              restockingFee: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '0.00',
+              },
+            },
           },
         },
       },
@@ -178,12 +388,42 @@ useSchemaOrg([
           description: "Le troisième album studio de Billie Eilish, \"HIT ME HARD AND SOFT\", sorti via Darkroom/Interscope Records, est son œuvre la plus audacieuse à ce jour, une collection de chansons à la fois diversifiée et cohérente - idéalement écoutée dans son intégralité du début à la fin - exactement comme le suggère le titre de l’album ; vous frappe fort et doucement, tant au niveau des paroles que du son, tout en contournant les genres et en défiant les tendances en cours de route. Avec l'aide de son frère et unique collaborateur, FINNEAS, le duo a écrit, enregistré et produit l'album ensemble dans leur ville natale de Los Angeles. Cet album fait suite à ses deux albums à succès « WHEN WE ALL FALL ASLEEP, WHERE DO WE GO ? » et « Happier Than Ever » et vise à développer davantage le monde de Billie Eilish.",
           sku: 'billie-hit-me',
           brand: 'Billie Eilish',
+          aggregateRating: {
+            '@type': 'AggregateRating',
+          },
+          review: {
+            '@type': 'Review',
+          },
           offers: {
             '@type': 'Offer',
             price: '34.99',
             priceCurrency: 'EUR',
             availability: 'InStock',
+            priceValidUntil: '2024-12-31',
             url: 'https://sensvinylo-store.com/vinyle/billie-hit-me',
+            shippingDetails: {
+              '@type': 'OfferShippingDetails',
+              deliveryTime: {
+                "@type": "ShippingDeliveryTime",
+                businessDays: "1-2",
+              },
+              cutoffTime: "12:00:15Z",
+              shippingRate: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '4.65',
+              },
+            },
+            hasMerchantReturnPolicy: {
+              '@type': 'MerchantReturnPolicy',
+              refundType: 'FullRefund',
+              returnPolicyCategory: 'InStoreReturnPolicy',
+              restockingFee: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '0.00',
+              },
+            },
           },
         },
       },
@@ -198,12 +438,42 @@ useSchemaOrg([
           description: "Obtenez une édition spéciale en vinyle 7\" des chansons à succès de The Weeknd",
           sku: 'the-weeknd-one-of-girls-popular',
           brand: 'The Weeknd',
+          aggregateRating: {
+            '@type': 'AggregateRating',
+          },
+          review: {
+            '@type': 'Review',
+          },
           offers: {
             '@type': 'Offer',
             price: '10.99',
             priceCurrency: 'EUR',
             availability: 'InStock',
+            priceValidUntil: '2024-12-31',
             url: 'https://sensvinylo-store.com/vinyle/the-weeknd-one-of-girls-popular',
+            shippingDetails: {
+              '@type': 'OfferShippingDetails',
+              deliveryTime: {
+                "@type": "ShippingDeliveryTime",
+                businessDays: "1-2",
+              },
+              cutoffTime: "12:00:15Z",
+              shippingRate: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '4.65',
+              },
+            },
+            hasMerchantReturnPolicy: {
+              '@type': 'MerchantReturnPolicy',
+              refundType: 'FullRefund',
+              returnPolicyCategory: 'InStoreReturnPolicy',
+              restockingFee: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '0.00',
+              },
+            },
           },
         },
       },
@@ -218,12 +488,42 @@ useSchemaOrg([
           description: "Ariana Grande fait partie de ces artistes précoces que les Etats Unis arrivent à mettre en lumière, comme avant elle Britney Spears ou Justin Timberlake, à travers les productions de sitcom Disney ou Nickelodeon. C’est donc très jeune que la jeune Ariana rencontre le succès mais surtout la rigueur qu’exige une vie rythmée par les tournages, les scènes et les opérations marketing intenses. Bien plus qu’un minois que l’on expose aux enfants d’outre-Atlantique, Mlle Grande a une voix et quelle voix ? C’est en 2013 que la carrière de la chanteuse va prendre un tournant définitif, quand elle sort son premier Album. Sortie fin 2020, « Position » est le 6e opus de l’artiste. Une fois de plus Ariana Grande met tout le monde d’accord et continue d’imposer au paysage Pop international, sa voix unique. Dans cet album la chanteuse s’appuie sur des productions efficaces et des featuring de poids tels que Dodja Cat, Ty Dolla $ign et The Weeknd. Retrouvez cette petite pépite Pop en édition vinyle couleur.",
           sku: 'ariana-positions',
           brand: 'Ariana Grande',
+          aggregateRating: {
+            '@type': 'AggregateRating',
+          },
+          review: {
+            '@type': 'Review',
+          },
           offers: {
             '@type': 'Offer',
             price: '19.99',
             priceCurrency: 'EUR',
             availability: 'InStock',
+            priceValidUntil: '2024-12-31',
             url: 'https://sensvinylo-store.com/vinyle/ariana-positions',
+            shippingDetails: {
+              '@type': 'OfferShippingDetails',
+              deliveryTime: {
+                "@type": "ShippingDeliveryTime",
+                businessDays: "1-2",
+              },
+              cutoffTime: "12:00:15Z",
+              shippingRate: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '4.65',
+              },
+            },
+            hasMerchantReturnPolicy: {
+              '@type': 'MerchantReturnPolicy',
+              refundType: 'FullRefund',
+              returnPolicyCategory: 'InStoreReturnPolicy',
+              restockingFee: {
+                '@type': 'MonetaryAmount',
+                currency: 'EUR',
+                value: '0.00',
+              },
+            },
           },
         },
       },
